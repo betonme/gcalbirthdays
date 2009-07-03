0a80e1871a0d4639e1758b7119b1f8abe64e5b1a
GlaserFrank <GlaserFrank@aa8f745a-674b-11de-ba0c-230037762ff9>
2009-07-03 17:45:09 +0000

    Added instructions

--- trunk/gCalBirthdays/src/GCalBirthdays.java
+++ trunk/gCalBirthdays/src/GCalBirthdays.java
@@ -1,13 +1,25 @@
-
-/* INSTRUCTION: This is a command line application. So please execute this template with the following arguments:
-
- arg[0] = username
- arg[1] = password
+/*  Copyright (c) 2009 Frank Glaser
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * @author (Frank Glaser)
- *
+ 
+/* INSTRUCTION: 
+ * This is a command line application. 
+ * So please execute this template with the following arguments:
+ * arg[0] = username as full mail address (i.e.: username@gmail.com)
+ * arg[1] = password
  */
 
 
